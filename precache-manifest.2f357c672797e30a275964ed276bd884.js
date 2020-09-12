8562cf0a9d73a8dd3ee5b93a2f1a94cb53d57919
atifaiman <alserembani94@gmail.com>
2020-09-12 19:38:06 +0800

    Updates

---
+++ precache-manifest.2f357c672797e30a275964ed276bd884.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8e06c60e446c8c178c137ba54a31a7",
+    "url": "/aivi-chat/index.html"
+  },
+  {
+    "revision": "c347b5222add5b4ad10c",
+    "url": "/aivi-chat/static/css/main.be0a96db.chunk.css"
+  },
+  {
+    "revision": "c0de9c0eddac92b5ccbf",
+    "url": "/aivi-chat/static/js/2.5ac21f02.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/aivi-chat/static/js/2.5ac21f02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c347b5222add5b4ad10c",
+    "url": "/aivi-chat/static/js/main.6b176206.chunk.js"
+  },
+  {
+    "revision": "a26740b1ffc503326b77",
+    "url": "/aivi-chat/static/js/runtime-main.c12e0f39.js"
+  },
+  {
+    "revision": "eb8779d864325522b9260791c5ebaaa8",
+    "url": "/aivi-chat/static/media/FontsFree-Net-Proxima-Nova-Black-It.eb8779d8.otf"
+  },
+  {
+    "revision": "0cd9a06ed7923acd0e769fa06775bef4",
+    "url": "/aivi-chat/static/media/FontsFree-Net-Proxima-Nova-Bold-It.0cd9a06e.otf"
+  },
+  {
+    "revision": "72b6f2c9990bd3d37b8013a59ed78902",
+    "url": "/aivi-chat/static/media/FontsFree-Net-Proxima-Nova-Bold.72b6f2c9.otf"
+  },
+  {
+    "revision": "efc2820473a48f510ce341184e5c2daf",
+    "url": "/aivi-chat/static/media/FontsFree-Net-Proxima-Nova-Light-It.efc28204.otf"
+  },
+  {
+    "revision": "ac51ad8a36d5bab6da11d454bcd06561",
+    "url": "/aivi-chat/static/media/FontsFree-Net-Proxima-Nova-Light.ac51ad8a.otf"
+  },
+  {
+    "revision": "c8708c7ae9f1ccfd893ee01efdf0ac51",
+    "url": "/aivi-chat/static/media/FontsFree-Net-Proxima-Nova-Reg-It.c8708c7a.otf"
+  },
+  {
+    "revision": "bf9f5d50c1b928ff21436517a1a95ad9",
+    "url": "/aivi-chat/static/media/FontsFree-Net-Proxima-Nova-Reg.bf9f5d50.otf"
+  },
+  {
+    "revision": "1e378969cd70396aaadd59f9a7a5ab18",
+    "url": "/aivi-chat/static/media/FontsFree-Net-Proxima-Nova-Sbold-It.1e378969.otf"
+  },
+  {
+    "revision": "f14eee643541cf03a10f26c944cc29f5",
+    "url": "/aivi-chat/static/media/FontsFree-Net-Proxima-Nova-Sbold.f14eee64.otf"
+  },
+  {
+    "revision": "fee25e2474a8b5bd3d65b1e3e53db243",
+    "url": "/aivi-chat/static/media/FontsFree-Net-Proxima-Nova-Thin-It.fee25e24.otf"
+  },
+  {
+    "revision": "284561febf8d2608625142c4c5a0be05",
+    "url": "/aivi-chat/static/media/FontsFree-Net-Proxima-Nova-Thin.284561fe.otf"
+  },
+  {
+    "revision": "fbc46aba2f5962dbadf744149b714741",
+    "url": "/aivi-chat/static/media/FontsFree-Net-Proxima-Nova-Xbold-It.fbc46aba.otf"
+  },
+  {
+    "revision": "5243e362912f7119ede836ab03f23ac7",
+    "url": "/aivi-chat/static/media/FontsFree-Net-Proxima-Nova-Xbold.5243e362.otf"
+  }
+]);
