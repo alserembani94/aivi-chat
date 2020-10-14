813cae8dd5b209bb7186a7519f62e4338b903638
tusen <tusen@qijang.com>
2020-10-14 17:32:14 +0800

    Cards will be automaticly created if the data is added and load more button

--- src/pages/SiteBlogList/index.tsx
+++ src/pages/SiteBlogList/index.tsx
@@ -1,174 +1,174 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Images } from '../../utils/Images';
 import '../../styles/SiteBlogList.scss';
 
 const SiteBlogList: React.FC = () => {
-    var multi = [[1, 2, 3], [23, 24, 25]];
-    console.log(multi[0][0]);
-    console.log(multi[0][1]);
-    console.log(multi[0][2]);
-    console.log(multi[1][0]);
-    console.log(multi[1][1]);
-    console.log(multi[1][2]);
+    var datas = [
+        {
+            img:Images.image_site_img1, 
+            feature:"FEATURED", 
+            topic:"DATA", 
+            title:"Financial World Trending", 
+            description:"Explore and test different ideas, themes, and styles instantly whether you want to experiment with your team. If that is not to your liking, you may try out other features we have offered.",
+            date:"28 August 2020"
+        }, 
+        {
+            img:Images.image_site_img2, 
+            feature:0, 
+            topic:"FINANCE", 
+            title:"Multiplayers in Finance", 
+            description:"Explore and test different ideas, themes, and styles instantly whether you want to experiment with your team. If that is not to your liking, you may try out other features we have offered.",
+            date:"28 August 2020"
+        },
+        {
+            img:Images.image_site_img3, 
+            feature:0, 
+            topic:0, 
+            title:"Charts are SkyRocketing", 
+            description:"Explore and test different ideas, themes, and styles instantly whether you want to experiment with your team. If that is not to your liking, you may try out other features we have offered.",
+            date:"28 August 2020"
+        },
+        {
+            img:Images.image_site_img4, 
+            feature:"FEATURED", 
+            topic:0, 
+            title:"Financial World with Aivi", 
+            description:"Explore and test different ideas, themes, and styles instantly whether you want to experiment with your team. If that is not to your liking, you may try out other features we have offered.",
+            date:"28 August 2020"
+        },
+        {
+            img:Images.image_site_img5, 
+            feature:0, 
+            topic:0, 
+            title:"Financial World with Aivi", 
+            description:"Explore and test different ideas, themes, and styles instantly whether you want to experiment with your team. If that is not to your liking, you may try out other features we have offered.",
+            date:"28 August 2020"
+        },
+        {
+            img:Images.image_site_img6, 
+            feature:0, 
+            topic:0, 
+            title:"Financial World with Aivi", 
+            description:"Explore and test different ideas, themes, and styles instantly whether you want to experiment with your team. If that is not to your liking, you may try out other features we have offered.",
+            date:"28 August 2020"
+        },
+        {
+            img:Images.image_site_img7, 
+            feature:0, 
+            topic:0, 
+            title:"Financial World with Aivi", 
+            description:"Explore and test different ideas, themes, and styles instantly whether you want to experiment with your team. If that is not to your liking, you may try out other features we have offered.",
+            date:"28 August 2020"
+        },
+        {
+            img:Images.image_site_img8, 
+            feature:0, 
+            topic:0, 
+            title:"Financial World with Aivi", 
+            description:"Explore and test different ideas, themes, and styles instantly whether you want to experiment with your team. If that is not to your liking, you may try out other features we have offered.",
+            date:"28 August 2020"
+        },
+        {
+            img:Images.image_site_img9, 
+            feature:0, 
+            topic:0, 
+            title:"Financial World with Aivi", 
+            description:"Explore and test different ideas, themes, and styles instantly whether you want to experiment with your team. If that is not to your liking, you may try out other features we have offered.",
+            date:"28 August 2020"
+        },
+        {
+            img:Images.image_site_img10, 
+            feature:0, 
+            topic:0, 
+            title:"Financial World with Aivi", 
+            description:"Explore and test different ideas, themes, and styles instantly whether you want to experiment with your team. If that is not to your liking, you may try out other features we have offered.",
+            date:"28 August 2020"
+        },
+    ];
+
+    const generatecard = (data : any, cardclass : string, quantity : string) => {
+        if(data === undefined){
+            return ""
+        }
+        let subCardClass = "SiteBlogList-Body-Content-"+ quantity +"Cards";
+        let subCardClass2 = "SiteBlogList-Body-Content-"+ quantity +"Cards-Card"
+        return <div className= {cardclass}>
+            <div className= {subCardClass}>
+                <div className={subCardClass2}>
+                    <img src={data.img} />
+                    {data.feature != 0?
+                        <div className="SiteBlogList-Body-Content-ImageContainer-Description"><div>{data.feature}</div></div>
+                    :""}
+                    <div className="SiteBlogList-Body-Content-CardDesc">
+                        <div className="SiteBlogList-Body-Content-CardDesc-Inner">
+                            <div>{data.topic}</div>
+                            <div>{data.title}</div>
+                            <div>{data.description}</div>
+                            <div>{data.date}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    }
+    const generateblogs = () => {
+        let ret = [];
+        while(datas.length>0){
+            if(datas.length>= 3){
+                if(ret.length % 2 == 0){
+                    ret.push(/* Row with 2 cards*/
+                        <div className="container">
+                            <div className="row">
+                                {generatecard(datas.shift(), "col-6","2")}
+                                {generatecard(datas.shift(), "col-6", "2")}
+                            </div>
+                        </div>)
+                }
+                else {
+                    ret.push(/* Row with 2 cards*/
+                        <div className="container">
+                            <div className="row">
+                                {generatecard(datas.shift(), "col-4","3")}
+                                {generatecard(datas.shift(), "col-4", "3")}
+                                {generatecard(datas.shift(), "col-4", "3")}
+                            </div>
+                        </div>)
+                }
+            } else if (datas.length >= 2){
+                ret.push(/* Row with 2 cards*/
+                    <div className="container">
+                        <div className="row">
+                            {generatecard(datas.shift(), "col-6","2")}
+                            {generatecard(datas.shift(), "col-6", "2")}
+                        </div>
+                    </div>)
+            } else {
+                ret.push(/* Row with 2 cards*/
+                    <div className="container">
+                        <div className="row">
+                            {generatecard(datas.shift(), "col-12","1")}
+                        </div>
+                    </div>)
+            }
+        }
+        return ret
+    }
+
+    
+    const [count, setCount] = useState(2);
+    
+    
     return (
         <React.Fragment>
+            
             <main className="SiteBlogList-Body">
                 <section className="SiteBlogList-Body-Section">
-              
                     <div className="SiteBlogList-Body-Content">
-                        <div className="SiteBlogList-Body-Content-Phase1">
-                            <div className="SiteBlogList-Body-Content-Phase1-Card1">
-                                <div className="SiteBlogList-Body-Content-ImageContainer">
-                                    <img src={Images.image_site_img1} />
-                                    <div className="SiteBlogList-Body-Content-ImageContainer-Description"><div>FEATURED</div></div>
-                                </div>
-                                <div className="SiteBlogList-Body-Content-CardDesc">
-                                    <div className="SiteBlogList-Body-Content-CardDesc-Inner">
-                                        <div>DATA</div>
-                                        <div>Financial World Trending</div>
-                                        <div>Explore and test different ideas, themes, and styles instantly whether you<br/>
-                                            want to experiment with your team. If that is not to your liking, you may<br/>
-                                            try out other features we have offered.
-                                        </div>
-                                        <div>28 August 2020</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="SiteBlogList-Body-Content-Phase1-Card2">
-                                <div>
-                                    <img src={Images.image_site_img2}/>
-                                </div>
-                                <div className="SiteBlogList-Body-Content-CardDesc">
-                                    <div className="SiteBlogList-Body-Content-CardDesc-Inner">
-                                        <div>FINANCE</div>
-                                        <div>Multiplayers in Finance</div>
-                                        <div>Explore and test different ideas, themes, and styles instantly whether you<br/>
-                                            want to experiment with your team. If that is not to your liking, you may<br/>
-                                            try out other features we have offered.
-                                        </div>
-                                        <div>28 August 2020</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="SiteBlogList-Body-Content-Phase2">
-                            <div className="SiteBlogList-Body-Content-Phase2-Card1">
-                                <div className="">
-                                    <img src={Images.image_site_img3} />
-                                </div>
-                                <div className="SiteBlogList-Body-Content-CardDesc">
-                                    <div className="SiteBlogList-Body-Content-CardDesc-Inner">
-                                        <div>Charts are SkyRocketing</div>
-                                        <div>
-                                            Explore and test different ideas, themes and,<br/>
-                                            styles instantly whether you want to experiment<br/>
-                                            with your team in a design sprint.
-                                        </div>
-                                        <div>28 August 2020</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="SiteBlogList-Body-Content-Phase2-Card2">
-                                <div className="SiteBlogList-Body-Content-ImageContainer">
-                                    <img src={Images.image_site_img4} />
-                                    <div className="SiteBlogList-Body-Content-ImageContainer-Description"><div>FEATURED</div></div>
-                                </div>
-                                <div className="SiteBlogList-Body-Content-CardDesc">
-                                    <div className="SiteBlogList-Body-Content-CardDesc-Inner">
-                                        <div>Financial World with Aivi</div>
-                                        <div>
-                                            Explore and test different ideas, themes and,<br/>
-                                            styles instantly whether you want to experiment<br/>
-                                            with your team in a design sprint.
-                                        </div>
-                                        <div>28 August 2020</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="SiteBlogList-Body-Content-Phase2-Card3">
-                                <div className="">
-                                    <img src={Images.image_site_img5} />
-                                </div>
-                                <div className="SiteBlogList-Body-Content-CardDesc">
-                                    <div className="SiteBlogList-Body-Content-CardDesc-Inner">
-                                        <div>Analyst Confirm Breakthrough</div>
-                                        <div>
-                                            Explore and test different ideas, themes and,<br/>
-                                            styles instantly whether you want to experiment<br/>
-                                            with your team in a design sprint.
-                                        </div>
-                                        <div>28 August 2020</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>  
-
-                        <div className="SiteBlogList-Body-Content-Phase3">
-                            <div className="SiteBlogList-Body-Content-Phase3-Card1">
-                                <div><img src={Images.image_site_img6} /></div>
-                                <div className="SiteBlogList-Body-Content-CardDesc">
-                                    <div className="SiteBlogList-Body-Content-CardDesc-Inner">
-                                        <div>Charts are SkyRocketing</div>
-                                        <div>Explore and test different ideas, themes and styles instantly wheather you want<br/>
-                                                to experiment with your team in a design sprint.
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="SiteBlogList-Body-Content-Phase3-Card2">
-                                <div><img src={Images.image_site_img7} /></div>
-                                <div className="SiteBlogList-Body-Content-CardDesc">
-                                    <div className="SiteBlogList-Body-Content-CardDesc-Inner">
-                                        <div>Charts are SkyRocketing</div>
-                                        <div>Explore and test different ideas, themes and styles instantly wheather you want<br/>
-                                                to experiment with your team in a design sprint.
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div className="SiteBlogList-Body-Content-Phase4">
-                            <div className="SiteBlogList-Body-Content-Phase4-Card1">
-                                <div><img src={Images.image_site_img8} /></div>
-                                <div className="SiteBlogList-Body-Content-CardDesc">
-                                    <div className="SiteBlogList-Body-Content-CardDesc-Inner">
-                                        <div>Charts are SkyRocketing</div>
-                                        <div>Explore and test different ideas, themes and styles instantly wheather you want<br/>
-                                                to experiment with your team in a design sprint.
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="SiteBlogList-Body-Content-Phase4-Card2">
-                                <div><img src={Images.image_site_img9} /></div>
-                                <div className="SiteBlogList-Body-Content-CardDesc">
-                                    <div className="SiteBlogList-Body-Content-CardDesc-Inner">
-                                        <div>Charts are SkyRocketing</div>
-                                        <div>Explore and test different ideas, themes and styles instantly wheather you want<br/>
-                                                to experiment with your team in a design sprint.
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="SiteBlogList-Body-Content-Phase4-Card3">
-                                <div><img src={Images.image_site_img10} /></div>
-                                <div className="SiteBlogList-Body-Content-CardDesc">
-                                    <div className="SiteBlogList-Body-Content-CardDesc-Inner">
-                                        <div>Charts are SkyRocketing</div>
-                                        <div>Explore and test different ideas, themes and styles instantly wheather you want<br/>
-                                                to experiment with your team in a design sprint.
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        
-
+                        {generateblogs().slice(0, count )}
                     </div>
                     <div className="SiteBlogList-Body-Section-Button">
-                        <button className="SiteBlogList-Body-Section-Button-RedButton">Load More</button>
+                        <button className="SiteBlogList-Body-Section-Button-RedButton" onClick={() => setCount(count + 2)}>Load More</button>
                     </div>
 
                 </section>
