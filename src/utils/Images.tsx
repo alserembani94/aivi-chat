5e8cc4fb35d41224a3f038a16326db08ccdbbde4
tusen <tusen@qijang.com>
2020-10-19 11:19:40 +0800

    Header Confict Fixed 2

--- src/utils/Images.tsx
+++ src/utils/Images.tsx
@@ -1,74 +1,3 @@
-<<<<<<< HEAD
-export const Images = {
-    icon_shopping: require('../assets/images/Icon-Shopping.svg'),
-    icon_utilities: require('../assets/images/Icon-Utilities.svg'),
-    icon_groceries: require('../assets/images/Icon-Groceries.svg'),
-    icon_education: require('../assets/images/Icon-Education.svg'),
-    icon_travel: require('../assets/images/Icon-Travel.svg'),
-    icon_entertainment: require('../assets/images/Icon-Entertainment.svg'),
-    icon_dining: require('../assets/images/Icon-Dining.svg'),
-    icon_petrol: require('../assets/images/Icon-Petrol.svg'),
-    icon_health: require('../assets/images/Icon-Health.svg'),
-    icon_send: require('../assets/images/Icon-Send.svg'),
-    icon_ticket: require('../assets/images/Icon-Ticket.svg'),
-    icon_readjust: require('../assets/images/Icon-Readjust.svg'),
-    icon_search: require('../assets/images/Icon-Search.svg'),
-
-    icon_facebook_small: require('../assets/images/Icon-Facebook-Small.png'),
-    icon_google_small: require('../assets/images/Icon-Google-Small.svg'),
-
-    icon_facebook_footer: require('../assets/images/Icon-Facebook.svg'),
-    icon_messanger_footer: require('../assets/images/Icon-Messanger.svg'),
-    icon_youtube_footer: require('../assets/images/Icon-YouTube.svg'),
-    icon_google_footer: require('../assets/images/Icon-Google.svg'),
-    icon_insta_footer: require('../assets/images/Icon-Instagram (filled).svg'),
-    icon_linkedin_footer: require('../assets/images/Icon-Linked In.svg'),
-
-    icon_maybank: require('../assets/images/logo-Maybank.svg'),
-    icon_RHBCard: require('../assets/images/RHB-Card.svg'),
-
-    image_submitted: require('../assets/images/Image-Submitted.svg'),
-
-    image_site_img1: require('../assets/images/Image-Site-Img1.svg'),
-    image_site_img2: require('../assets/images/Image-Site-Img2.svg'),
-    image_site_img3: require('../assets/images/Image-Site-Img3.svg'),
-    image_site_img4: require('../assets/images/Image-Site-Img4.svg'),
-    image_site_img5: require('../assets/images/Image-Site-Img5.svg'),
-    image_site_img6: require('../assets/images/Image-Site-Img6.svg'),
-    image_site_img7: require('../assets/images/Image-Site-Img7.svg'),
-    image_site_img8: require('../assets/images/Image-Site-Img8.svg'),
-    image_site_img9: require('../assets/images/Image-Site-Img9.svg'),
-    image_site_img10: require('../assets/images/Image-Site-Img10.svg'),
-
-    
-    image_home_img1: require('../assets/images/Image-Home-Img1.svg'),
-    image_home_mobile: require('../assets/images/Image-Home-Mobile.png'),
-
-    button_apple_download: require('../assets/images/Button-Apple-Download.svg'),
-    button_android_download: require('../assets/images/Button-Android-Download.svg'),
-    button_apple_download_white: require('../assets/images/Button-Apple-Download-White.svg'),
-    button_android_download_white: require('../assets/images/Button-Android-Download-White.svg'),
-
-    image_article_img1: require('../assets/images/Image-Article-Img1.svg'),
-    image_article_img2: require('../assets/images/Image-Article-Img2.svg'),
-    image_article_img3: require('../assets/images/Image-Article-Img3.svg'),
-    image_article_img4: require('../assets/images/Image-Article-Img4.svg'),
-    image_article_img5: require('../assets/images/Image-Article-Img5.svg'),
-
-    image_article_arrow_to_right: require('../assets/images/Image-Article-ArrowToRight.svg'),
-    image_article_vector: require('../assets/images/Image-Article-Vector.svg'),
-
-    icon_fb_img: require('../assets/images/Icon-Fb-Img.svg'),
-    icon_google_img: require('../assets/images/Icon-Google-Img.svg'),
-
-    logo_AIVI: require('../assets/images/Logo-AIVI.svg'),
-    logo_AIVI_white: require('../assets/images/Logo-AIVI-White.svg'),
-
-    arrow_right: require('../assets/images/arrow-right.svg'),
-    arrow_right_white: require('../assets/images/Icon-Arrow.svg'),
-
-    emoji_malaysia: require('../assets/images/malaysia-emoji.png'),
-=======
 export const Images = {
     icon_shopping: require('../assets/images/Icon-Shopping.svg'),
     icon_utilities: require('../assets/images/Icon-Utilities.svg'),
@@ -144,5 +73,4 @@ export const Images = {
     socialLogin_googleplus: require('../assets/images/SocialLogin-GooglePlus.svg'),
     socialLogin_linkedin: require('../assets/images/SocialLogin-Linkedin.svg'),
 
->>>>>>> 79591d6e8dc827d66645aac1433cceb5272a332d
 }
